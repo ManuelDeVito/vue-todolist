8eee58802492f1e3134ceff0df43065afc4d86c9
Manuel <manuel.devito@icloud.com>
2020-11-11 11:56:22 +0100

    Array stampata in pagina

---
+++ main.js
@@ -0,0 +1,22 @@
+// definire una lista di todo e stamparli in pagina in un elenco.
+// predisporre un input testuale per aggiungere un nuovo todo: al click di un pulsante, il testo inserito nell'input viene aggiunto alla lista dei todo
+// al click su un item della lista, il todo viene rimosso
+
+var app = new Vue ({
+
+    el: '#root',
+
+    data: {
+
+        lista: ['pane', 'latte', 'mozzarella', 'pasta', 'carne']
+    },
+
+    methods: {
+
+        
+    }
+
+
+
+
+});
